697232d56b7cf1f95bca0b3c729b08afae1a3a47
Carlos Eduardo Costa <carloseduardocostadomont@gmail.comm>
2025-03-18 12:04:47 -0300

    sortear amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("OLÁ, MUNDO!")
+let listaDeAmigos = [];
+
+function adicionarNome() {
+    let nome = document.getElementById('amigo').value;
+    if (nome === "") {
+        alert("DIGITE UM NOME VÁLIDO");
+        return;
+    }
+    listaDeAmigos.push(nome);
+    document.getElementById('listaAmigos').innerHTML = listaDeAmigos.join('<br>');
+    document.getElementById('amigo').value = "";
+}
+
+function sortear() {
+    if (listaDeAmigos.length === 0) {
+        alert("DIGITE UM NOME VÁLIDO");
+        return;
+    }
+    let sorteio = Math.floor(Math.random() * listaDeAmigos.length);
+    document.getElementById('resultado').innerHTML = `Sorteado: ${listaDeAmigos[sorteio]}`;
+}
